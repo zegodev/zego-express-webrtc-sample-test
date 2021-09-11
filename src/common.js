5594c85a0900b3b3e98312a68e8d25d1430c6532
keenwang <keenwang@zego.im>
2021-09-11 12:53:21 +0800

    🚀  自动获取token

--- src/common.js
+++ src/common.js
@@ -377,11 +377,28 @@ async function login(roomId) {
     userID = $("#custom-userID").val() || userID;
     // 获取token需要客户自己实现，token是对登录房间的唯一验证
     // Obtaining a token needs to be implemented by the customer. The token is the only verification for the login room.
-    let token = $("#custom-token").val() || "";
+    let token = "";
     //测试用，开发者请忽略
     //Test code, developers please ignore
-    if (token) {
-
+    if ($("#custom-token").val()) {
+        const res = await $.ajax({
+            url: 'https://dev-rangeaudio.zego.cloud/thirdToken/get',
+            type: "POST",
+            data: JSON.stringify({
+                "version": "03",
+                "appId": appID,
+                "idName": userID,
+                "roomId": roomId,
+                "privilege": {
+                    "1": 1,
+                    "2": 1
+                },
+                "expire_time": parseInt($("#custom-token").val())
+            }),
+            dataType: "json",
+            contentType: "application/json; charset=utf-8"
+        })
+        token = res.data.token;
     } else if (cgiToken) {
         token = await $.get(tokenUrl, {
             app_id: appID,
