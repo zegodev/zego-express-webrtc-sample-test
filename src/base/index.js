ab3abf077a9bd5520006b59aa3e9bf490c318f3c
keenwang <keenwang@zego.im>
2022-12-27 20:03:43 +0800

    🐛  update

--- src/base/index.js
+++ src/base/index.js
@@ -538,9 +538,9 @@ $(async () => {
             track
         );
         if (cameraStreamVideoTrack) {
-            if (!result.errorCode && result.codecameraStreamVideoTrack !== track) {
-                track.stop();
-            }
+            // if (!result.errorCode && result.codecameraStreamVideoTrack !== track) {
+            //     track.stop();
+            // }
             cameraStreamVideoTrack.stop();
             cameraStreamVideoTrack = null
         }
@@ -577,9 +577,9 @@ $(async () => {
             track
         );
         if (micAudioTrack) {
-            if (!result.errorCode && micAudioTrack !== track) {
-                track.stop();
-            }
+            // if (!result.errorCode && micAudioTrack !== track) {
+            //     track.stop();
+            // }
             micAudioTrack.stop();
             micAudioTrack = null
         }
